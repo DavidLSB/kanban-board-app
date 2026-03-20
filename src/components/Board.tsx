5ae5a99ed03f74f7f75d79e74611cff24d88062a
David_LSB <dsanchezb@fi.uba.ar>
2026-03-20 03:43:53 +0000

    Render basic columns with simple layout in Board component

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,23 @@
 function Board() {
-    return <div>Board</div>
+    const columns = ["To Do", "Doing", "Done"] //temporarely hard coded
+    return (
+        <div 
+            style={{
+            display: "flex", 
+            gap: "20px", 
+            border: "2px solid gray",
+            padding: "10px",
+            width: "1000px"
+        }}>
+            {columns.map((column) => (
+                <div key={column} style={{
+                    border: "1px solid gray",
+                }}>
+                    <h2>{column}</h2>
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default Board
