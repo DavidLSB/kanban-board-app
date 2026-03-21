82dc43e52829dfcf9bdcd07d5d8a380b071c7899
David_LSB <dsanchezb@fi.uba.ar>
2026-03-21 01:28:11 +0000

    Display basic task cards inside columns

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -4,12 +4,25 @@ type ColumnProps = {
 }
 
 function Column({title, width}: ColumnProps) {
+    const tasks = ["Task 1", "Task 2"] // hardcoded por ahora
+
     return (
         <div style={{
             border: "1px solid gray",
-            width: `${width}px`
+            width: `${width}px`,
+            padding: "10px"
         }}>
             <h2>{title}</h2>
+
+            {tasks.map((task) => (
+                <div key={task} style={{
+                    border: "1px solid black",
+                    marginTop: "10px",
+                    padding: "5px"
+                }}>
+                    {task}
+                </div>
+            ))}
         </div>
     )
 }
