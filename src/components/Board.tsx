2d02754a7d7cd3f8fd41ceb88dab214a93ab8f4c
David_LSB <dsanchezb@fi.uba.ar>
2026-03-22 01:25:43 +0000

    Add basic task creation with button

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -7,14 +7,14 @@ function Board() {
     {
         title: "To Do",
         tasks: [
-            { id: "1", title: "Task A" },
-            { id: "2", title: "Task B" }
+            { id: "1", title: "Task 1" },
+            { id: "2", title: "Task 2" }
         ]
     },
     {
         title: "Doing",
         tasks: [
-            { id: "3", title: "Task C" }
+            { id: "3", title: "Task 3" }
         ]
     },
     {
@@ -22,20 +22,37 @@ function Board() {
         tasks: []
     }
     ])
+    function addTask() {
+        const newTask = {
+            id: Date.now().toString(),
+            title: `Task ${columns[0].tasks.length + 2}`
+        }
+
+        const newColumns = [...columns]
+        newColumns[0].tasks.push(newTask) // temporarely always on 'To Do' column
+
+        setColumns(newColumns)
+    }
+
     return (
-        <div 
-            style={{
-            display: "flex", 
-            gap: "20px", 
-            border: "2px solid gray",
-            padding: "10px",
-            width: "1000px"
-        }}>
-            {columns.map((column) => (
-                <Column tasks={column.tasks} key={column.title} title={column.title} width={300} />
-            ))}
+        <div>
+            <div 
+                style={{
+                display: "flex", 
+                gap: "20px", 
+                border: "2px solid gray",
+                padding: "10px",
+                width: "1000px"
+            }}>
+                {columns.map((column) => (
+                    <Column tasks={column.tasks} key={column.title} title={column.title} width={300} />
+                ))}
+            </div>
+            <button onClick={addTask} style = {{ marginBottom: "10px" }}>Add Task</button>
         </div>
     )
 }
+
+
 
 export default Board
