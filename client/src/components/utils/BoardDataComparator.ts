5a5db9d3213825e222b207a63a4f439eb7c7f3d9
David_LSB <dsanchezb@fi.uba.ar>
2026-08-31 04:58:55 +0000

    deploy frontend UX fix (automatically use server ids on data conflict)

--- client/src/components/utils/BoardDataComparator.ts
+++ client/src/components/utils/BoardDataComparator.ts
@@ -1,3 +1,20 @@
+function onlyIdsDiffer(localData:any, serverData:any) {
+    if (!localData || !serverData) {
+        return false
+    }
+    if (localData.title !== serverData.title) return false
+    if (localData.columns.length !== serverData.columns.length) return false
+    for (let i = 0; i < localData.columns.length; i++) {
+        if (localData.columns[i].title !== serverData.columns[i].title) return false
+        if (localData.columns[i].tasks.length !== serverData.columns[i].tasks.length) return false
+        for (let j = 0; j < localData.columns[i].tasks.length; j++) {
+            if (localData.columns[i].tasks[j].title !== serverData.columns[i].tasks[j].title) return false
+            if (localData.columns[i].tasks[j].description !== serverData.columns[i].tasks[j].description) return false
+        }
+    }
+    return true
+}
+
 export function processBoardDataComparison(serverData: any, isConflictSolved: boolean) {
     const localData = localStorage.getItem("board-data")
     if (isConflictSolved) {
@@ -17,7 +34,12 @@ export function processBoardDataComparison(serverData: any, isConflictSolved: bo
                 columns: serverData.columns
             }
             const differs = JSON.stringify(localComparisonTarget) !== JSON.stringify(serverComparisonTarget)
+            if (!onlyIdsDiffer(localComparisonTarget, serverComparisonTarget)) {
                 return {...serverData, hasConflict: differs}
+            } else {
+                localStorage.setItem("board-data", JSON.stringify(serverData))
+                return {...serverData, hasConflict: false}
+            }
         } catch {
             return {...serverData, hasConflict: true}
         }
